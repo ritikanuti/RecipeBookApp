22e647edb8e37f20e900cf86824c369e961fc4de
Lenorris A <lenorris.hughes.jp@gmail.com>
2025-04-10 17:06:32 +0900

    Create example_Recipe.py
    This is a recipe example with tags

---
+++ example_Recipe.py
@@ -0,0 +1,10 @@
+example_recipe = {
+    "name": "Pasta",
+    "prep_time": 15,  # in minutes
+    "cook_time": 20,  # in minutes
+    "ingredients": [
+        {"name": "Pasta", "quantity": 200, "unit": "grams"},
+        {"name": "Tomato Sauce", "quantity": 1, "unit": "cup"},
+        {"name": "Garlic", "quantity": 2, "unit": "cloves"}
+    ]
+}
