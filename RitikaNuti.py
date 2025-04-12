1be5e74f3d805a51713670eeb9322f55823d3901
ritikanuti <112502900+ritikanuti@users.noreply.github.com>
2025-04-12 20:39:06 +0900

    Update RitikaNuti.py

--- RitikaNuti.py
+++ RitikaNuti.py
@@ -1,1 +1,4 @@
-print("Lenorris was here")
+# Make user menu with options
+import Ahmad Ikram
+int(input("Choose a number for the following options.\n1. Add recipe\n2. Edit Recipe\n3. Delete recipe\n4. List all recipes"))
+
