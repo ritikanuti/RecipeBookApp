727d3eb0db41fc147ccf150273e4c0b17690d645
ritikanuti <112502900+ritikanuti@users.noreply.github.com>
2025-04-12 21:12:43 +0900

    Update RitikaNuti.py

--- RitikaNuti.py
+++ RitikaNuti.py
@@ -1,4 +1,8 @@
 # Make user menu with options
-import Ahmad Ikram
-int(input("Choose a number for the following options.\n1. Add recipe\n2. Edit Recipe\n3. Delete recipe\n4. List all recipes"))
+import Ahmad_Ikram
+#user chooses option from menu
+choice = int(input("Choose a number for the following options.\n1. Add recipe\n2. Edit Recipe\n3. Delete recipe\n4. List all recipes"))
+if(choice == 1):
+  Ahmad_Ikram.add_recipe(
+  
 
