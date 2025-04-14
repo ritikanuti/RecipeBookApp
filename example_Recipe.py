a8aac04ca1cf3716c0382be21e796a20e673864c
Lenorris A <lenorris.hughes.jp@gmail.com>
2025-04-14 13:26:36 +0900

    Update example_Recipe.py
    Added cuisine type for search functions

--- example_Recipe.py
+++ example_Recipe.py
@@ -1,10 +1,13 @@
 example_recipe = {
     "name": "Pasta",
-    "prep_time": 15,  # in minutes
-    "cook_time": 20,  # in minutes
+    "prep_time": 15,
+    "cook_time": 20,
     "ingredients": [
         {"name": "Pasta", "quantity": 200, "unit": "grams"},
         {"name": "Tomato Sauce", "quantity": 1, "unit": "cup"},
         {"name": "Garlic", "quantity": 2, "unit": "cloves"}
-    ]
+    ],
+    "cuisine": "Italian",
+    "vegetarian": True,
+    "meal_type": "Dinner"  # or Breakfast, Lunch, Snack, etc.
 }
