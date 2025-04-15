73110148d4c7b0016ff2a2e3a04df7fafcbf2679
ritikanuti <112502900+ritikanuti@users.noreply.github.com>
2025-04-15 12:35:10 +0900

    Update RitikaNuti.py

--- RitikaNuti.py
+++ RitikaNuti.py
@@ -1,14 +1,16 @@
 # Make user menu with options
 import Ahmad_Ikram
+import Lenorris
+recipes = []
 
 file2 = open("recipes.txt", 'r')
-data = file2.read()
-recipes = data
-print(recipes)
+data = file2.readlines()
+file2.close()
+
 #user chooses option from menu
 
 while True:
-  choice = int(input("Choose a number for the following options.\n1. Add recipe\n2. Edit Recipe\n3. Delete recipe\n4. List all recipes\n5. exit menu"))
+  choice = int(input("Choose a number for the following options.\n1. Add recipe\n2. Edit Recipe\n3. Delete recipe\n4. List all recipes\n5. Search specific recipe\n6. Exit menu"))
   if(choice == 1):
     Ahmad_Ikram.add_recipe(recipes)
   elif(choice == 2):
@@ -17,16 +19,17 @@ while True:
     Ahmad_Ikram.delete_recipe(recipes)
   elif(choice == 4):
     Ahmad_Ikram.list_all_recipes(recipes)
-  elif(choice == 5):
+  elif(choice == 6):
     break
 
-
-
 #write recipes to file
-file1 = open("recipes.txt", 'w')
-recipeStr = str(recipes)
-file1.write(recipeStr)
+file1 = open("recipes.txt", 'w+')
+numLines = len(file1.readlines())
+for i in range(1,numLines):
+  file1.write(recipes[i])
 file1.close()
+
+
 
 
   
